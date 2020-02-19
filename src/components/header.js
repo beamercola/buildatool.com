a231bc0c3c5f81ab579651dcc72ddbc93c86c5f0
Beamer <beamercola@gmail.com>
2020-02-18 17:15:28 -0800

    Initial Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,21 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header className="flex items-center justify-between container py-4">
+    <h1 className="font-semibold text-3xl">build a tool</h1>
+    <div className="uppercase font-medium">
+      <Link className="mx-4" to="/about">
+        About
+      </Link>
+      <Link className="mx-4" to="/faq">
+        FAQ
+      </Link>
+      <Link className="mx-4" to="/pricing">
+        Pricing
+      </Link>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="text-center p-12">
+      <h1 className="text-6xl font-bold leading-none">
+        Get more warm leads
+        <br />
+        Keep more clients.
+      </h1>
+      <p className="text-xl leading-snug font-medium p-6 py-8">
+        From warm lead to long-term client,
+        <br />
+        utilizing custom tools is the way to go
+      </p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <div className="bg-gray-200 flex flex-col border border-gray-400 rounded-lg">
+      <div className="px-4 py-2 text-gray-600 flex items-center">
+        <ul className="flex mr-2">
+          <li className="rounded-full h-4 w-4 bg-red-600 mr-2"></li>
+          <li className="rounded-full h-4 w-4 bg-yellow-500 mr-2"></li>
+          <li className="rounded-full h-4 w-4 bg-green-500 mr-2"></li>
+        </ul>
+        https://yourwebsite.com
+      </div>
+      <img
+        className="h-64 object-cover"
+        src="https://images.unsplash.com/photo-1545389336-cf090694435e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80"
+        alt=""
+      />
+      <div className="text-center p-8 px-24">
+        <div className="max-w-md mx-auto">
+          <h3 className="text-4xl">Is Hot Yoga Right For You</h3>
+          <p className="">
+            Answer these few questions to learn more about our type of yoga. Who
+            knows, there might be something special waiting for you at the
+            end...
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
